48662864ff27e167dec86c29a66f491d94e6ef86
durga <durgaprasadkampali@gmail.com>
2024-08-10 12:28:04 +0530

    latest

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: "AIzaSyBFZNcvlIZqz_i_kec5zdGM0xeqt5sLwX8",
+  authDomain: "dashboard-a1fb6.firebaseapp.com",
+  projectId: "dashboard-a1fb6",
+  storageBucket: "dashboard-a1fb6.appspot.com",
+  messagingSenderId: "466895635521",
+  appId: "1:466895635521:web:199d7fb02c791058c832c0",
+  measurementId: "G-VN3VMB4QX7",
 };
 
 let firebase_app;
