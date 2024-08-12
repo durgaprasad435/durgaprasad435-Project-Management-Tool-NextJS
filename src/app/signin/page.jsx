ad2e7d299b325f2c5be651c50fd81e06fbc12833
durga <durgaprasadkampali@gmail.com>
2024-08-12 08:08:26 +0530

    latest

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -41,7 +41,7 @@ function Signin() {
   const auth = getAuth(firebase_app);
   const toast = useToast();
   const router = useRouter();
-  const { setuserEmail, setAccesstoken } = useAuth();
+  //const { setuserEmail, setAccesstoken } = useAuth();
   let errorsObject = {
     EmailMessage: "",
     PasswordMessage: "",
