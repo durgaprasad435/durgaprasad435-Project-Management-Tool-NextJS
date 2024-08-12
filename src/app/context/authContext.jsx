31a4ea9409f3fbcb675f9e904a21a3a332dc6822
durga <durgaprasadkampali@gmail.com>
2024-08-12 13:45:23 +0530

    latest build

--- src/app/context/authContext.jsx
+++ src/app/context/authContext.jsx
@@ -16,7 +16,7 @@ export const AuthProvider = ({ children }) => {
       setuserEmail(authorizedUser.userEmail);
       setAccesstoken(authorizedUser.accesstoken);
     } else {
-      router.push("/signin");
+      router.push("/");
     }
   });
   return (
